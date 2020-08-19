4c8fd7dff6a138bec43acafaa0e7f79d75c70422
khalil311 <69592747+khalil311@users.noreply.github.com>
2020-08-19 16:40:03 +0100

    CheckPoint

--- New Unity Project (1)/Assets/Scripts/Flap.cs
+++ New Unity Project (1)/Assets/Scripts/Flap.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Flap : MonoBehaviour
 {
+    public Text text;
+    public float scorenumber;
+    public string score;
+    public GameObject start;
+    
     public bool isDead;
     private Rigidbody2D rd2d;
     [SerializeField]
@@ -13,6 +20,7 @@ public class Flap : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 0;
         rd2d = GetComponent<Rigidbody2D>();
         rd2d.velocity = Vector2.right * speed * Time.deltaTime;
     }
@@ -31,5 +39,27 @@ public class Flap : MonoBehaviour
     {
         isDead = true;
         rd2d.velocity = Vector2.zero;
+        GetComponent<Animator>().SetBool("isDead", true);
+    }
+    public void Replay()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Unfreeze()
+    {
+        Time.timeScale = 1;
+    }
+    public void removebutton()
+    {
+        start.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "score")
+        {
+            scorenumber++;
+            Debug.Log(scorenumber);
+        }
+        text.text = scorenumber.ToString();
     }
 }
---
+++ New Unity Project (1)/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public float minY;
+    public float maxY;
+    public float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            float obstacleY = Random.Range(minY, maxY);
+            Vector3 spawnPosition = new Vector3(transform.position.x + distance, obstacleY, 0);
+            collision.gameObject.transform.position = spawnPosition;
+        }
+        
+    }
+}
